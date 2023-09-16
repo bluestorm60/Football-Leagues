2cd72de6a77f3952f845ef8454e1828723756615
Ahmed shafik <a.mahmuod@gmail.com>
2023-09-16 18:37:22 +0300

    entities has been added

--- Football Leagues/MainFeature/Domain/Models/TeamsUIModel.swift
+++ Football Leagues/MainFeature/Domain/Models/TeamsUIModel.swift
@@ -21,18 +21,11 @@ struct TeamsUIModel {
     }
 
     struct TeamUIModel {
-        let area: AreaUIModel
         let id: Int
         let name: String
         let shortName: String
         let crest: String
 
-        struct AreaUIModel {
-            let id: Int
-            let name: String
-            let code: String
-            let flag: String
-        }
     }
 }
 
@@ -58,7 +51,6 @@ extension TeamsUIModel.CompetitionUIModel {
 
 extension TeamsUIModel.TeamUIModel {
     init(from team: TeamsResponseModel.Team) {
-        self.area = AreaUIModel(from: team.area)
         self.id = team.id
         self.name = team.name
         self.shortName = team.shortName
@@ -66,11 +58,4 @@ extension TeamsUIModel.TeamUIModel {
     }
 }
 
-extension TeamsUIModel.TeamUIModel.AreaUIModel {
-    init(from area: TeamsResponseModel.Team.Area) {
-        self.id = area.id
-        self.name = area.name
-        self.code = area.code
-        self.flag = area.flag
-    }
-}
+
